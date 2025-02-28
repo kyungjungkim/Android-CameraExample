f4667cff49ab16dde1e30987116628da491fc9c5
kyungjungkim <kyungjung.kim@gmail.com>
2025-02-28 12:22:37 +0900

    2025. 02. 28.

--- app/src/main/java/kr/co/kkj/cameraexample/MainActivity.java
+++ app/src/main/java/kr/co/kkj/cameraexample/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -15,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,19 +37,17 @@ public class MainActivity extends AppCompatActivity {
         Button btnpicture = findViewById(R.id.btn_camera);
         imageView = findViewById(R.id.imageview1);
 
-        btnpicture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, REQUEST_CODE);
-            }
+        btnpicture.setOnClickListener(v -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, REQUEST_CODE);
         });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (REQUEST_CODE == requestCode && RESULT_OK == resultCode) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            assert data != null;
+            Bitmap photo = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
             imageView.setImageBitmap(photo);
         } else {
             Toast.makeText(this, "cancelled", Toast.LENGTH_SHORT).show();
